a8c696f9d1d464c345adbcde891cc023d7c5d40f
Bambang Hermawan <blackblizt@gmail.com>
2017-11-10 18:12:42 +0700

    fix UI

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -4,17 +4,17 @@ $this->load->view('template/head');
 
 <!--tambahkan custom css disini-->
 <!-- iCheck -->
-<link href="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/iCheck/flat/blue.css') ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/iCheck/flat/blue.css') ?>" rel="stylesheet" type="text/css" />
 <!-- Morris chart -->
-<link href="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/morris/morris.css') ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/morris/morris.css') ?>" rel="stylesheet" type="text/css" />
 <!-- jvectormap -->
-<link href="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/jvectormap/jquery-jvectormap-1.2.2.css') ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/jvectormap/jquery-jvectormap-1.2.2.css') ?>" rel="stylesheet" type="text/css" />
 <!-- Date Picker -->
-<link href="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/datepicker/datepicker3.css') ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/datepicker/datepicker3.css') ?>" rel="stylesheet" type="text/css" />
 <!-- Daterange picker -->
-<link href="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/daterangepicker/daterangepicker-bs3.css') ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/daterangepicker/daterangepicker-bs3.css') ?>" rel="stylesheet" type="text/css" />
 <!-- bootstrap wysihtml5 - text editor -->
-<link href="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') ?>" rel="stylesheet" type="text/css" />
 
 <?php
 $this->load->view('template/topbar');
@@ -91,11 +91,6 @@ $this->load->view('admin/template/sidebar');
         </div><!-- ./col -->
     </div><!-- /.row -->
     <!-- Main row -->
-    <div class="row">
-        <!-- Left col -->
-        
-</section><!-- /.content -->
-
 
 <?php
 $this->load->view('template/js');
@@ -110,20 +105,25 @@ $this->load->view('template/js');
 </script>
 <!-- Morris.js charts -->
 <script src="<?php echo base_url('assets/js/raphael-min.js') ?>"></script>
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/morris/morris.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/morris/morris.min.js') ?>" type="text/javascript"></script>
 <!-- Sparkline -->
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/sparkline/jquery.sparkline.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/sparkline/jquery.sparkline.min.js') ?>" type="text/javascript"></script>
 <!-- jvectormap -->
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') ?>" type="text/javascript"></script>
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') ?>" type="text/javascript"></script>
 <!-- jQuery Knob Chart -->
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/knob/jquery.knob.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/knob/jquery.knob.js') ?>" type="text/javascript"></script>
 <!-- daterangepicker -->
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/daterangepicker/daterangepicker.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/daterangepicker/daterangepicker.js') ?>" type="text/javascript"></script>
 <!-- datepicker -->
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/datepicker/bootstrap-datepicker.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/datepicker/bootstrap-datepicker.js') ?>" type="text/javascript"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>" type="text/javascript"></script>
 <!-- iCheck -->
-<script src="<?php echo base_url('assets/AdminLTE-2.4.2/plugins/iCheck/icheck.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/iCheck/icheck.min.js') ?>" type="text/javascript"></script>
 
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/dist/js/pages/dashboard.js') ?>" type="text/javascript"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/dist/js/demo.js') ?>" type="text/javascript"></script>
